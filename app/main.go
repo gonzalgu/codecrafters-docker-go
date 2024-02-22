e2809aea6fce750039278b3cc8602d790d73870c
gonzalgu <gadol.gonzalez@gmail.com>
2024-02-22 11:17:22 -0300

    filesystem isolation

--- app/main.go
+++ app/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
+	"syscall"
+
 	// Uncomment this block to pass the first stage!
 	"os"
 	"os/exec"
@@ -12,11 +15,54 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	// create tmp directory
+	tmp_dir, err := os.MkdirTemp("", "sandbox_*")
+	if err != nil {
+		fmt.Printf("Error creating tmp dir: %v\n", err)
+		os.Exit(1)
+	}
+	defer os.Remove(tmp_dir)
+
+	// chmod 0755 temp directory
+	err = os.Chmod(tmp_dir, 0755)
+	if err != nil {
+		fmt.Printf("Error chmod: %v\n", err)
+		os.Exit(1)
+	}
+
+	// mkDirAll filepath.join(tmp_dir, /usr/local/bin) 0755
+	err = os.MkdirAll(filepath.Join(tmp_dir, "/usr/local/bin"), 0755)
+	if err != nil {
+		fmt.Printf("Error mkdirall: %v\n", err)
+		os.Exit(1)
+	}
+
+	// os.Link(docker-explorer full path, filepathjoin(tempDir, "/usr/local/bin", "docker-explorer"))
+	err = os.Link("/usr/local/bin/docker-explorer", filepath.Join(tmp_dir, "/usr/local/bin", "docker-explorer"))
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	//chroot temp_dir
+	err = syscall.Chroot(tmp_dir)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	//chdir into /
+	err = os.Chdir("/")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
 	cmd := exec.Command(command, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		if exiterr, ok := err.(*exec.ExitError); ok {
